8d4d3fce399e0f27f1f9c7305dff76aa3dc059fc
Arni-27 <arnialus@gmail.com>
2026-02-13 10:45:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
-import streamlit as st 
+import streamlit as st
+import math
 
 st.title("_hitung_ is :blue[luas lingkaran] :rocket:")
-r = st.number_input("masukan jari-jari (cm):",0)
+
+r = st.number_input("Masukan Jari-Jari (cm):",0)
+t = st. numbet_input("Masukan Tinggi (cm) : ", 0) 
+
 if st.button("hitung luas lingkaran", type="primary"):
    v = math.pi*(r**2)
+st.succes(f'Volume tabung adalah {v:2f}')
