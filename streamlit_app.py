db39e3f2e0797279e5e19e62bf2ccc73e1c66b37
Arni-27 <arnialus@gmail.com>
2026-02-13 10:34:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("_hitung_ is :blue[luas lingkaran] :rocket:")
 r = st.number_input("masukan jari-jari (cm):",0)
 if st.button("hitung luas lingkaran", type="primary"):
-v = math.pi*(r**2)
+   v = math.pi*(r**2)
